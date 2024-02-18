7aad4dbbf4db61a3e282e0a38245b39f5e7cb1bc
Andrel <andreltd312@gmail.com>
2024-02-18 18:03:07 -0300

    User base class and resourse

---
+++ src/main/java/com/andrelucs/sweb/SpringwebApplication.java
@@ -0,0 +1,13 @@
+package com.andrelucs.sweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrelucs/sweb/SpringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelucs.sweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
